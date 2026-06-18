568e1d43e27063ddc3b682644ba625338c9f25ca
gorefilip20 <gorefilip20@gmail.com>
2026-06-18 04:21:16 +0100

    fix: restore valid esm export default

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // Dangerously allow production builds to successfully complete even if
-    // your project has TypeScript errors.
     ignoreBuildErrors: true,
   },
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
